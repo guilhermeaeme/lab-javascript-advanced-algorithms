b60c4ea2ca6e969e568ed48f859e6e719907627d
guilhermeaeme <guilhermeaeme@gmail.com>
2019-01-16 21:28:30 -0200

    StackDataStructure

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,30 @@
 function StackDataStructure () {
+	this.stackControl = [];
+	this.MAX_SIZE = 2;
+
+	this.isEmpty = function() {
+		return (this.stackControl.length == 0);
+	};
+
+	this.canPush = function() {
+		return (this.stackControl.length < this.MAX_SIZE);
+	};
+
+	this.pop = function(item) {
+		if(this.stackControl.length == 0) {
+			return 'Stack Underflow';
+		}
+
+		return this.stackControl.pop(item);
+	};
+
+	this.push = function(item) {
+		if(this.canPush()) {
+			this.stackControl.push(item);
+
+			return this.stackControl;
+		} else {
+			return 'Stack Overflow';
+		}
+	}
 }
