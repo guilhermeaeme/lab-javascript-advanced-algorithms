1197f106ecf129eee276a33dd9ee8e5fcb445d07
guilhermeaeme <guilhermeaeme@gmail.com>
2019-01-16 19:35:57 -0200

    QueueDataStructure

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,38 @@
 function QueueDataStructure () {
+	this.queueControl = [];
+	this.MAX_SIZE = 2;
+
+	this.isEmpty = function() {
+		return (this.queueControl.length == 0);
+	};
+
+	this.canEnqueue = function() {
+		return (this.queueControl.length < this.MAX_SIZE);
+	};
+
+	this.queue = function(item) {
+		if(this.queueControl.length == 0) {
+			return 'Queue Underflow';
+		}
+
+		return this.queueControl.pop(item);
+	};
+
+	this.enqueue = function(item) {
+		if(this.canEnqueue()) {
+			this.queueControl.unshift(item);
+
+			return this.queueControl;
+		} else {
+			return 'Queue Overflow';
+		}
+	};
+
+	this.dequeue = function() {
+		if(this.queueControl.length == 0) {
+			return 'Queue Underflow';
+		}
+
+		return this.queueControl[this.queueControl.length - 1];
+	}
 }
